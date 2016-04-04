c06c41f219e253f502ce6787494f1da1145ea24c
pejuan <jose.pejuan@unitec.edu>
2016-04-03 21:52:33 -0600

    Agregando segunda implementacion

---
+++ SAT/src/sat/Alumno.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sat;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jose Alberto Pejuan
+ */
+public class Alumno {
+    private String nombre;
+    private String numCuenta;
+    private ArrayList<Clase> clasesMatriculadas = new ArrayList();
+    private ArrayList<String> horasOcupado = new ArrayList();
+    private ArrayList<Clase> clasesPreferidas = new ArrayList();
+    private float satisfecho = 5;
+    public Alumno(String nombre, String numCuenta) {
+        this.nombre = nombre;
+        this.numCuenta = numCuenta;
+    }
+    
+    public Alumno() {
+        
+    }
+
+    @Override
+    public String toString() {
+        String añadido = "";
+        for (int i = 0; i < clasesMatriculadas.size(); i++) {
+            añadido += "Clase:"+clasesMatriculadas.get(i).getNombre()+"  Hora:"+clasesMatriculadas.get(i).getHora()+"\n";
+        }
+        return "Alumno{" + "nombre=" + nombre + ", numCuenta=" + numCuenta+"{"+añadido+"}"+ "Satisfacion(1-5):"+satisfecho;
+    }
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public ArrayList<Clase> getClasesMatriculadas() {
+        return clasesMatriculadas;
+    }
+
+    public void setClasesMatriculadas(ArrayList<Clase> clasesMatriculadas) {
+        this.clasesMatriculadas = clasesMatriculadas;
+    }
+
+    public ArrayList<String> getHorasOcupado() {
+        return horasOcupado;
+    }
+
+    public void setHorasOcupado(ArrayList<String> horasOcupado) {
+        this.horasOcupado = horasOcupado;
+    }
+    
+    public void addHorasOcupado(String hora){
+        this.horasOcupado.add(hora);
+    }
+    public void addClase(Clase objclase){
+        this.clasesMatriculadas.add(objclase);
+        horasOcupado.add(objclase.getHora());
+    }
+
+    public ArrayList<Clase> getClasesPreferidas() {
+        return clasesPreferidas;
+    }
+
+    public void setClasesPreferidas(ArrayList<Clase> clasesPreferidas) {
+        this.clasesPreferidas = clasesPreferidas;
+    }
+    public void addClasePreferida(Clase objclase){
+        this.clasesPreferidas.add(objclase);
+    }
+
+    public float getSatisfecho() {
+        return satisfecho;
+    }
+
+    public void reducirSatisfecho(float satisfecho) {
+        this.satisfecho -= satisfecho;
+    }
+    public void verificarSeguidas(){
+        
+        ArrayList<Integer> nuevasHoras = new ArrayList();
+        for (int i = 0; i < horasOcupado.size(); i++) {
+            if (horasOcupado.get(i).equals("7:00")) {
+                nuevasHoras.add(0);
+            }else if(horasOcupado.get(i).equals("8:30")){
+                nuevasHoras.add(1);
+            }else if(horasOcupado.get(i).equals("10:10")){
+                nuevasHoras.add(2);
+            }else if(horasOcupado.get(i).equals("11:30")){
+                nuevasHoras.add(3);
+            }else if(horasOcupado.get(i).equals("13:00")){
+                nuevasHoras.add(4);
+            }else if(horasOcupado.get(i).equals("14:20")){
+                nuevasHoras.add(5);
+            }else if(horasOcupado.get(i).equals("15:40")){
+                nuevasHoras.add(6);
+            }else if(horasOcupado.get(i).equals("17:10")){
+                nuevasHoras.add(7);
+            }else if(horasOcupado.get(i).equals("18:30")){
+                nuevasHoras.add(8);
+            }
+        }
+        nuevasHoras.sort(null);
+        int anterior = -1;
+        for (int i = 0; i < nuevasHoras.size(); i++) {
+            if ((anterior+1)<nuevasHoras.get(i) && nuevasHoras.size()>1) {
+                satisfecho -= 1;
+                break;
+            }else{
+                anterior = nuevasHoras.get(i);
+            }
+        }
+        if (nuevasHoras.size()==0) {
+            satisfecho = 0;
+        }
+    }
+    
+}
---
+++ SAT/src/sat/Clase.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sat;
+
+/**
+ *
+ * @author Jose Alberto Pejuan
+ */
+public class Clase {
+    private int capacidad = 20;
+    private String nombre;
+    private String codigoClase;
+    private String facultad;
+    private String hora;
+    private int alumnosPosibles = 0;
+    private String numAula;
+    private Maestro profesor;
+    private boolean abierta = false;
+    private boolean tieneprof = false;
+    private int location;
+    private Aula aula;
+
+    public Clase(String nombre, String codigoClase, String facultad) {
+        this.nombre = nombre;
+        this.codigoClase = codigoClase;
+        this.facultad = facultad;
+    }
+    
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    public String toString2(){
+        return "Clase: "+nombre + codigoClase + getFacultad() + hora + numAula + profesor.toString();
+    }
+    
+    public Clase(String nombre, String hora) {
+        this.nombre = nombre;
+        this.hora = hora;
+    }
+
+    public Clase(String nombre) {
+        this.nombre = nombre;
+    }
+    
+
+    public Clase() {
+    }
+    
+    
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    public void reducirCapacidad(){
+        this.capacidad--;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getNumAula() {
+        return numAula;
+    }
+
+    public void setNumAula(String numAula) {
+        this.numAula = numAula;
+    }
+
+    public boolean isAbierta() {
+        return abierta;
+    }
+
+    public void setAbierta(boolean abierta) {
+        this.abierta = abierta;
+    }
+
+    public String getCodigoClase() {
+        return codigoClase;
+    }
+
+    public void setCodigoClase(String codigoClase) {
+        this.codigoClase = codigoClase;
+    }
+
+    public Maestro getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Maestro profesor) {
+        this.profesor = profesor;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public Aula getAula() {
+        return aula;
+    }
+
+    public void setAula(Aula aula) {
+        this.aula = aula;
+        numAula = Integer.toString(aula.getNum());
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public boolean isTieneprof() {
+        return tieneprof;
+    }
+
+    public void setTieneprof(boolean tieneprof) {
+        this.tieneprof = tieneprof;
+    }
+
+    public int getAlumnosPosibles() {
+        return alumnosPosibles;
+    }
+
+    public void setAlumnosPosibles(int alumnosPosibles) {
+        this.alumnosPosibles = alumnosPosibles;
+    }
+    public void agregarAlumnoPosible(){
+        alumnosPosibles++;
+    }
+    
+}
